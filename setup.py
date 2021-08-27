2dbeb324046f45861f551c887fa908546e80d45c
chrishavlin <chris.havlin@gmail.com>
2021-08-27 15:19:28 -0500

    setup.py linted

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-import setuptools
 import os
+
+import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 reqd_pkgs = [
-             "yt>=4.0", 
-             "xmltodict",
-            ] 
+    "yt>=4.0",
+    "xmltodict",
+]
 
 setuptools.setup(
     name="yt_aspect",
@@ -15,13 +16,11 @@ setuptools.setup(
     author="Chris Havlin",
     author_email="chris.havlin@gmail.com",
     description="A yt plugin for loading ASPECT output",
-    install_requires = reqd_pkgs,
+    install_requires=reqd_pkgs,
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/chrishavlin/yt_aspect",
-    project_urls={
-        "Bug Tracker": "https://github.com/chrishavlin/yt_aspect/issues",
-    },
+    project_urls={"Bug Tracker": "https://github.com/chrishavlin/yt_aspect/issues",},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
