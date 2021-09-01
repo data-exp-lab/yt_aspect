35827667f7604858e7bb01a05f22f6cf96a94131
Chris Havlin <chris.havlin@gmail.com>
2021-09-01 16:06:35 -0500

    isort

--- yt_aspect/__init__.py
+++ yt_aspect/__init__.py
@@ -1,5 +1,5 @@
+from .data_structures import ASPECTDataset  # noqa: F401
 from .data_structures import ASPECTUnstructuredIndex  # noqa: F401
 from .data_structures import ASPECTUnstructuredMesh  # noqa: F401
-from .data_structures import ASPECTDataset  # noqa: F401
 from .fields import ASPECTFieldInfo  # noqa: F401
 from .io import IOHandlerASPECT  # noqa: F401
