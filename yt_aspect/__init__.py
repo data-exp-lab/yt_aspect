c007dd79e2059987f89120cde1b9337ea4de802c
Chris Havlin <chris.havlin@gmail.com>
2021-09-01 16:04:01 -0500

    fix imports

--- yt_aspect/__init__.py
+++ yt_aspect/__init__.py
@@ -1,8 +1,5 @@
-from yt.utilities.object_registries import output_type_registry
-
 from .data_structures import ASPECTUnstructuredIndex  # noqa: F401
 from .data_structures import ASPECTUnstructuredMesh  # noqa: F401
-from .data_structures import ASPECTDataset
+from .data_structures import ASPECTDataset  # noqa: F401
 from .fields import ASPECTFieldInfo  # noqa: F401
 from .io import IOHandlerASPECT  # noqa: F401
-
